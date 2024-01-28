5a389d75ea8c514f0a1f9b8eb96865ad57967496
AWS ParallelCluster user <ec2-user@ip-10-200-77-190.ec2.internal>
2024-01-28 19:03:06 +0000

    added functionality to output ligandability profile

--- py/grid_pruner.py
+++ py/grid_pruner.py
@@ -7,6 +7,7 @@ from glob import glob
 from pymol import cmd, CmdException
 import itertools as it
 
+
 def logic(index, skip):
         """ check modulus """
         if index % skip == 0:
@@ -23,18 +24,42 @@ def check_file(file):
         except:
             return False
 
+def make_profile(profile_name, obj="resn UNK and name C", rna_obj = 'complex', cutoff = 5):
+    """ 
+    Generates a ligandability profile, i.e., ligandability as a function of residue position    
+    """
+    atoms1 = cmd.get_model(obj)
+    profile = {"%s"%(at.resi): 0 for at in cmd.get_model("name C1' and polymer.nucleic and %s"%(rna_obj)).atom}
+    print(profile)
+    for at in atoms1.atom:
+        sele = "br. polymer.nucleic and %s within %s of (resn %s and resi %s and name %s)"%(rna_obj, cutoff+2, at.resn, at.resi, at.name)       
+        atoms2 = cmd.get_model(sele)
+        ligandability = at.b
+        sasa = at.q
+        for bt in atoms2.atom:          
+            resi = bt.resi
+            distance = np.sqrt((at.coord[0]-bt.coord[0])**2 +  (at.coord[1]-bt.coord[1])**2 + (at.coord[2]-bt.coord[2])**2)
+            sij = ligandability*np.exp(-(distance/cutoff)**2)
+            profile[resi] += sij
+            #print(at.q, sele)
+            #if distance < cutoff: print(distance, sele)
+    print(profile)  
+    df = pd.DataFrame.from_records([{"residue": key, "ligandability": profile[key]} for key in profile.keys()])
+    df.to_csv(profile_name, index=False)
+    return(profile)
+
 def remove_isolated(obj="resn UNK and name C", cutoff = 5):
-	atoms = cmd.get_model(obj)
-	for at in atoms.atom:
-		distances = []
-		for bt in atoms.atom:
-			distance = np.sqrt((at.coord[0]-bt.coord[0])**2 +  (at.coord[1]-bt.coord[1])**2 + (at.coord[2]-bt.coord[2])**2)
-			if distance != 0.0 and distance < cutoff: distances.append(distance)
-		if len(distances) < 4:
-			sele = "resn %s and resi %s and name %s"%(at.resn, at.resi, at.name)
-			print(sele)
-			cmd.remove(sele)
-			atoms = cmd.get_model(obj)
+    atoms = cmd.get_model(obj)
+    for at in atoms.atom:
+        distances = []
+        for bt in atoms.atom:
+            distance = np.sqrt((at.coord[0]-bt.coord[0])**2 +  (at.coord[1]-bt.coord[1])**2 + (at.coord[2]-bt.coord[2])**2)
+            if distance != 0.0 and distance < cutoff: distances.append(distance)
+        if len(distances) < 4:
+            sele = "resn %s and resi %s and name %s"%(at.resn, at.resi, at.name)
+            print(sele)
+            cmd.remove(sele)
+            atoms = cmd.get_model(obj)
 
 def prune_grid(rna, score_file, outname, quantile = 0.99, sasa_cutoff = 20.0):
     # make sure all atoms within an object occlude one another
@@ -78,16 +103,21 @@ def prune_grid(rna, score_file, outname, quantile = 0.99, sasa_cutoff = 20.0):
 
     # remove isolated
     remove_isolated()
-
+        
     # write out grid file
     coor = "%s_pruned_grid_clusters.xyz"%(outname)
     xyz = cmd.get_coords('tmpPoint', 1)
     df = pd.DataFrame.from_records(xyz)
     df.insert(0, "element", "C")
-    df.to_csv(coor, index = False, header = False, sep = " ")
-
-    # write out complex
+    df.to_csv(coor, index = False, header = False, sep = " ")    
+    
+    # generate ligandability profile
     cmd.create("complex", "%s tmpPoint"%rna)
+    profile = make_profile("%s_profile.csv"%(outname), rna_obj = 'complex')
+    cmd.alter("(polymer.nucleic)", 'b=0.0')
+    [cmd.alter("(polymer.nucleic and resi %s)"%(key), 'b=%6.3f'%(profile[key])) for key in profile.keys()]
+    
+    # write out complex
     coor = "%s_pruned_grid_clusters.pdb"%(outname)
     cmd.save(coor, "complex")
     coor = "cavity_pruned_grid.sd"
